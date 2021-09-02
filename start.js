514bbcc6b9fd4e87378140665ade6380e7203624
Anon Muangwijit <anonmuangwijit@gmail.com>
2021-09-02 16:12:50 +0700

    create function getDistamce

--- start.js
+++ start.js
@@ -4,6 +4,9 @@
   canvas.height = window.innerHeight;
   const context = canvas.getContext('2d');
   let previousPoint = { x: 0, y: 0 }; 
+  function getDistance(previousPoint, currentPoint) { 
+      return Math.sqrt((previousPoint.x - currentPoint.x) ** 2 + (previousPoint.y - currentPoint.y) ** 2);
+  }
   function run() {
   }
   run();
