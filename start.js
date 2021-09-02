4599e82960b3914c9f8f7008de3e84c76210013a
Anon Muangwijit <anonmuangwijit@gmail.com>
2021-09-02 16:09:22 +0700

    echo previousPoint

--- start.js
+++ start.js
@@ -3,6 +3,7 @@
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   const context = canvas.getContext('2d');
+  let previousPoint = { x: 0, y: 0 }; 
   function run() {
   }
   run();
