c0d016907400919f529dc5faa9b9697c3f2360f6
Anon Muangwijit <anonmuangwijit@gmail.com>
2021-09-02 16:15:08 +0700

    create function onMousemove for control pen

--- start.js
+++ start.js
@@ -7,6 +7,29 @@
   function getDistance(previousPoint, currentPoint) { 
       return Math.sqrt((previousPoint.x - currentPoint.x) ** 2 + (previousPoint.y - currentPoint.y) ** 2);
   }
+  function onMouseMove({ pageX, pageY }) {
+    const currentPoint = { x: pageX, y: pageY };
+
+    context.beginPath();
+
+    context.lineCap = 'round'; 
+    context.lineJoin = 'round'; 
+      
+    
+    context.lineWidth = Math.random() / distance * 40;  
+     
+    const distance = getDistance(previousPoint, currentPoint); 
+    const opacity = Math.min(0.5, 1 / distance);
+    context.strokeStyle = `rgba(222, 10, 109, ${opacity})`;
+
+    context.moveTo(previousPoint.x, previousPoint.y); 
+    context.lineTo(previousPoint.x, previousPoint.y);
+
+    context.stroke(); 
+    context.closePath(); 
+     
+    previousPoint = currentPoint;
+  } 
   function run() {
   }
   run();
